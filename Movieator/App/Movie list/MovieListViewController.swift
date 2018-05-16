6cba6dd88f993466de7691ae6d97bd313d83ee4c
Matej Korman <matejkorman@matejs-mbp>
2018-05-16 10:20:48 +0200

    Created UI.

--- Movieator/App/Movie list/MovieListViewController.swift
+++ Movieator/App/Movie list/MovieListViewController.swift
@@ -8,8 +8,40 @@
 
 import UIKit
 
-class MovieListViewController : UIViewController {
+class MovieListViewController: UIViewController {
+    let reuseIdentifier = "cell"
+    var titles = ["Movie1", "Movie2", "Movie3", "Movie4", "Movie5", "Movie6", "Movie7", "Movie8"]
+    var years = ["2002", "2015", "2016", "1986", "1997", "2006", "2011", "2008"]
+
     override func viewDidLoad() {
         navigationItem.backBarButtonItem = nil
+    }
+}
+
+extension MovieListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MovieCollectionViewCell
+        
+        cell.titleLabel.text = self.titles[indexPath.item]
+        cell.yearLabel.text = self.years[indexPath.item]
+        cell.backgroundColor = UIColor.cyan
+        
+        return cell
+    }
+    
+    
+}
+
+extension MovieListViewController: UICollectionViewDelegate {
+    
+}
+
+extension MovieListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (self.view.frame.size.width - 30) / 2, height: (self.view.frame.size.width - 30) / 2)
     }
 }
---
+++ Movieator/Common/UI/MovieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionViewCell.swift
+//  Movieator
+//
+//  Created by Matej Korman on 16/05/2018.
+//  Copyright © 2018 Codeopolius. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+}
