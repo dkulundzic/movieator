4b0aedfb79fdadf744ec78f809136d03340d3b43
Matej Korman <matejkorman@matejs-mbp>
2018-05-17 12:05:06 +0200

    Add searching bar and MovieSearchViewController

---
+++ Movieator/App/Movie search/MovieSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MovieSearchViewController.swift
+//  Movieator
+//
+//  Created by Matej Korman on 17/05/2018.
+//  Copyright © 2018 Codeopolius. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchViewController: UIViewController {
+    
+}
