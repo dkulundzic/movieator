5519a33a1af296762b341d659072f29c0db7599b
Matej Korman <matejkorman@matejs-mbp>
2018-05-17 17:18:46 +0200

    Implement Movie Search feature.

--- Movieator/App/Movie search/MovieSearchViewController.swift
+++ Movieator/App/Movie search/MovieSearchViewController.swift
@@ -9,5 +9,48 @@
 import UIKit
 
 class MovieSearchViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
     
+    private var filteredMovies = [Movie]()
+    let reuseIdentifier = "cell"
+
+    func filterMovies(movies: [Movie], with query: String) {
+        filteredMovies = movies.filter { movies in movies.title.lowercased().contains(query.lowercased()) }
+        collectionView.reloadData()
+    }
+}
+
+// MARK: - Data Source Extension
+extension MovieSearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.filteredMovies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MovieCollectionViewCell
+        
+        let movie = filteredMovies[indexPath.item]
+        cell.titleLabel.text = movie.title
+        let year = String(Calendar.current.component(.year, from: movie.releaseDate))
+        cell.yearLabel.text = year
+        
+        let posterFetcher = MoviePosterFetcher()
+        posterFetcher.fetchMoviePoster(with: movie.poster,
+                                       success: { (data) in
+                                        let image = UIImage(data: data)
+                                        cell.imageView.image = image
+        },
+                                       failure: { (error) in
+                                        print("Error getting poster, \(error)")
+        })
+        return cell
+    }
+}
+
+// MARK: - Delegate Flow Layout Extension
+extension MovieSearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (self.view.frame.size.width - 30) / 2
+        return CGSize(width: size, height: size)
+    }
 }
