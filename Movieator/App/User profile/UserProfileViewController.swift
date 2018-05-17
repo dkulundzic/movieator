fff0478b9769f4b0146ec1279b6280e216e2bd30
Matej Korman <matejkorman@matejs-mbp>
2018-05-17 08:33:54 +0200

    Add MovieDetailsViewController and UserProfileViewController.

---
+++ Movieator/App/User profile/UserProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewController.swift
+//  Movieator
+//
+//  Created by Matej Korman on 16/05/2018.
+//  Copyright © 2018 Codeopolius. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+    
+}
